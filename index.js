f4301faa7cadc02f60ce098420d3a52e0bdc0c5b
Jirachai <premji04607@gmail.com>
2023-01-05 20:50:48 +0700

    copy all file to github/webapp

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const dbConfig = require('./config/mongodb.config.js')
+const Customer = require('./models/customer.js')
+
+const cors = require('cors')
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}))
+
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.url)
+    .then(()=>{
+        Customer.deleteMany({},(err)=>{
+    if(err){
+        process.exit();
+           }
+           console.log('Remove Collection of Customer')
+           initCustomer();
+        });
+
+    }).catch(err=>{
+        console.log('Cannt Connect to MongoDB')
+        process.exit();
+    })
+
+app.use(cors())
+require('./routes/customer.route.js')(app);
+
+const server = app.listen(3000, ()=>{
+    let port = server.address().port
+    console.log('Run at http://localhost:%s', port)
+})
+
+function initCustomer(){
+    let data = [
+        {
+            CustomerId: 1001,
+            Fullname: "Krisada",
+            Address: "Nonthaburi"
+        },
+        {
+            CustomerId: 1002,
+            Fullanme: "Prem",
+            Address: "Bangkok"
+        },
+        {
+            CustomerId: 1003,
+            Fullname: "Aom",
+            Address: "Samutprakan"
+        }
+    ]
+    for(let i=0; i<data.length; i++){
+        const c =new Customer(data[i]);
+        c.save()
+    }
+    console.log("สร้างข้อมูล Customer สำเร็จแล้ว")
+}
+
